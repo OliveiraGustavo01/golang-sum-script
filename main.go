a1af3705ea9d53f32b535f3ab31a88d3c8bacfcb
Oliveira_Gustavo01 <gustavooliveira6466@outlook.com>
2025-01-29 17:30:12 -0300

    Update main.go

--- main.go
+++ main.go
@@ -4,16 +4,38 @@ import "fmt"
 
 func main() {
 	var x, y int
+	var op string
 
 	fmt.Print("First number, pleas: ")
 	fmt.Scanln(&x)
 
 	fmt.Print("Second number, pleas: ")
 	fmt.Scanln(&y)
-	soma := calc(x, y)
-	fmt.Println("Your sum is", soma)
+
+	fmt.Print("Choose an operation (+, -, *, /): ")
+	fmt.Scanln(&op)
+
+	result := calc(x, y, op)
+	fmt.Println("Your result is:", result)
 }
 
-func calc(x int, y int) int {
-	return x + y
+func calc(x int, y int, op string) int {
+	switch op {
+	case "+":
+		return x + y
+	case "-":
+		return x - y
+	case "*":
+		return x * y
+	case "/":
+		if y != 0 {
+			return x / y
+		} else {
+			fmt.Println("Error: division by zero")
+			return 0
+		}
+	default:
+		fmt.Println("Invalid operation")
+		return 0
+	}
 }
